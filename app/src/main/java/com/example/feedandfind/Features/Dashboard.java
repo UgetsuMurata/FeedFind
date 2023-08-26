d5c0a67d8b05a121f850bb10a3a2c74222f2d0cb
Angelo Echano <grey12angelo@gmail.com>
2023-08-26 23:54:31 +0800

    adding toolbar in the dashboard
    NOTE:  not yet finished (colors & functions)

--- app/src/main/java/com/example/feedandfind/Features/Dashboard.java
+++ app/src/main/java/com/example/feedandfind/Features/Dashboard.java
@@ -1,7 +1,10 @@
 package com.example.feedandfind.Features;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.example.feedandfind.R;
@@ -12,5 +15,9 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.features_dashboard);
+
+        //Toolbar customToolbar = findViewById(R.id.custom_toolbar);
+        //setSupportActionBar(customToolbar);
+
     }
 }
