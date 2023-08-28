80213b837ee8a319d06fa8bc057d1d299cac82e5
Kai Yaneza <92856337+yaneza-teamlight@users.noreply.github.com>
2023-08-28 14:50:53 +0800

    - initial layout of features_gpsmap.xml

---
+++ app/src/main/java/com/example/feedandfind/Features/GPSMap.java
@@ -0,0 +1,16 @@
+package com.example.feedandfind.Features;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.feedandfind.R;
+
+public class GPSMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.features_gpsmap);
+    }
+}
