90fd84a4ff47a7302e86ea7c851f414c49aca363
Kai Yaneza <92856337+yaneza-teamlight@users.noreply.github.com>
2023-09-05 07:14:10 +0800

    - GPSMap: Added multiple features.

--- app/src/main/java/com/example/feedandfind/Features/GPSMap.java
+++ app/src/main/java/com/example/feedandfind/Features/GPSMap.java
@@ -1,16 +1,38 @@
 package com.example.feedandfind.Features;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.feedandfind.R;
 
 public class GPSMap extends AppCompatActivity {
 
+    LinearLayout cornerMenu;
+    ConstraintLayout cornerMenuClose;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.features_gpsmap);
+
+        cornerMenu = findViewById(R.id.corner_menu);
+        cornerMenuClose = findViewById(R.id.corner_menu_close_button);
+
+        cornerMenu.setVisibility(View.GONE);
+        cornerMenuClose.setVisibility(View.GONE);
+        findViewById(R.id.corner_menu_button).setOnClickListener(view -> {
+            cornerMenu.setVisibility(View.VISIBLE);
+            cornerMenuClose.setVisibility(View.VISIBLE);
+        });
+        cornerMenuClose.setOnClickListener(view -> {
+            cornerMenu.setVisibility(View.GONE);
+            cornerMenuClose.setVisibility(View.GONE);
+        });
     }
+
+
 }
