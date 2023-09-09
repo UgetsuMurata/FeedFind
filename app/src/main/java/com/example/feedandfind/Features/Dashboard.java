9030582819103a4408ba6a42034f3f5332d91458
Angelo Echano <grey12angelo@gmail.com>
2023-09-09 09:16:38 +0800

    Navigation Drawer and Dashboard
    Navigation Drawer Fixed
    Dashboard Ongoing

--- app/src/main/java/com/example/feedandfind/Features/Dashboard.java
+++ app/src/main/java/com/example/feedandfind/Features/Dashboard.java
@@ -1,20 +1,25 @@
 package com.example.feedandfind.Features;
 
 import android.os.Bundle;
+import android.view.Window;
 
 import com.example.feedandfind.NavigationMenu.NavigationDrawer;
 import com.example.feedandfind.R;
+import com.example.feedandfind.databinding.ActivityNavigationDrawerBinding;
 import com.example.feedandfind.databinding.FeaturesDashboardBinding;
 
 
 public class Dashboard extends NavigationDrawer {
 
-    FeaturesDashboardBinding activityNavigationDrawerBinding;
+    FeaturesDashboardBinding featuresDashboardBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        activityNavigationDrawerBinding = FeaturesDashboardBinding.inflate(getLayoutInflater());
-        setContentView(activityNavigationDrawerBinding.getRoot());
+        featuresDashboardBinding = FeaturesDashboardBinding.inflate(getLayoutInflater());
+        setContentView(featuresDashboardBinding.getRoot());
     }
+
+
+
 }
--- app/src/main/java/com/example/feedandfind/NavigationMenu/NavigationDrawer.java
+++ app/src/main/java/com/example/feedandfind/NavigationMenu/NavigationDrawer.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
@@ -20,7 +21,7 @@ public class NavigationDrawer extends AppCompatActivity implements NavigationVie
 
     public void setContentView(View view){
         drawerLayout = (DrawerLayout) getLayoutInflater().inflate(R.layout.activity_navigation_drawer, null);
-        FrameLayout container = drawerLayout.findViewById(R.id.dashboardContainer);
+        FrameLayout container = drawerLayout.findViewById(R.id.activityContainer);
         container.addView(view);
         super.setContentView(drawerLayout);
 
@@ -31,6 +32,20 @@ public class NavigationDrawer extends AppCompatActivity implements NavigationVie
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.open,R.string.close);
         drawerLayout.addDrawerListener(toggle);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.setDrawerIndicatorEnabled(false); // Disable the default hamburger icon
+        toggle.setHomeAsUpIndicator(R.drawable.custom_icon); // Set your custom icon
+        toggle.setToolbarNavigationClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Handle custom icon click here (e.g., open/close the drawer)
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                } else {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+            }
+        });
         toggle.syncState();
 
         navigationView.setNavigationItemSelectedListener(this);
