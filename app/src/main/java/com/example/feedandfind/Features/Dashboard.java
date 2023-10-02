2a436153e04634bf37399b508f57038385edd29a
Murata Ugetsu <92856337+UgetsuMurata@users.noreply.github.com>
2023-10-02 21:08:53 +0800

    - Reverted to pre-conflict commit.

--- app/src/main/java/com/example/feedandfind/Features/Dashboard.java
+++ app/src/main/java/com/example/feedandfind/Features/Dashboard.java
@@ -1,6 +1,7 @@
 package com.example.feedandfind.Features;
 
 import android.os.Bundle;
+import android.view.Window;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,6 +10,7 @@ import com.example.feedandfind.Adapter.RecordAdapter;
 import com.example.feedandfind.Model.RecordModel;
 import com.example.feedandfind.NavigationMenu.NavigationDrawer;
 import com.example.feedandfind.R;
+import com.example.feedandfind.databinding.ActivityNavigationDrawerBinding;
 import com.example.feedandfind.databinding.FeaturesDashboardBinding;
 
 import java.util.ArrayList;
