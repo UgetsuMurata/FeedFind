ed85ae3fa53dd80a05957c40ff1cc03415d48fba
hyrumbravo <118159809+hyrumbravo@users.noreply.github.com>
2023-12-18 02:04:57 +0800

    updated diet, working na yung timepicker pero dun sa line pindutin para lumabas time hahaha

--- app/src/main/java/com/example/feedandfind/FeederConfiguration/Feeder_Configuration_Individual_Diet.java
+++ app/src/main/java/com/example/feedandfind/FeederConfiguration/Feeder_Configuration_Individual_Diet.java
@@ -1,11 +1,15 @@
 package com.example.feedandfind.FeederConfiguration;
 
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +17,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.feedandfind.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.Calendar;
+
 public class Feeder_Configuration_Individual_Diet extends AppCompatActivity {
+
+    EditText meal1;
+    EditText meal2;
+    EditText meal3;
+    TimePickerDialog timePickerDialog;
+    Calendar calendar;
+    int currentHour;
+    int currentMinute;
+    String amPm;
+
+    Button cancelbtn;
+
 
     String[] items =  {"Once a day","Twice a day","Thrice a day"};
     AutoCompleteTextView autoCompleteTxt;
@@ -37,6 +55,87 @@ public class Feeder_Configuration_Individual_Diet extends AppCompatActivity {
             }
         });
 
+        meal1 = findViewById(R.id.first_meal);
+        meal1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar = Calendar.getInstance();
+                currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+                currentMinute = calendar.get(Calendar.MINUTE);
 
+                timePickerDialog = new TimePickerDialog(Feeder_Configuration_Individual_Diet.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+                        if (hourOfDay >= 12) {
+                            amPm = "PM";
+                        } else {
+                            amPm = "AM";
+                        }
+                        meal1.setText(String.format("%02d:%02d", hourOfDay, minutes) + amPm);
+                    }
+                }, currentHour, currentMinute, false);
+
+                timePickerDialog.show();
+            }
+        });
+
+        meal2 = findViewById(R.id.second_meal);
+        meal2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar = Calendar.getInstance();
+                currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+                currentMinute = calendar.get(Calendar.MINUTE);
+
+                timePickerDialog = new TimePickerDialog(Feeder_Configuration_Individual_Diet.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+                        if (hourOfDay >= 12) {
+                            amPm = "PM";
+                        } else {
+                            amPm = "AM";
+                        }
+                        meal2.setText(String.format("%02d:%02d", hourOfDay, minutes) + amPm);
+                    }
+                }, currentHour, currentMinute, false);
+
+                timePickerDialog.show();
+            }
+        });
+
+
+        meal3 = findViewById(R.id.third_meal);
+        meal3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar = Calendar.getInstance();
+                currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+                currentMinute = calendar.get(Calendar.MINUTE);
+
+                timePickerDialog = new TimePickerDialog(Feeder_Configuration_Individual_Diet.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+                        if (hourOfDay >= 12) {
+                            amPm = "PM";
+                        } else {
+                            amPm = "AM";
+                        }
+                        meal3.setText(String.format("%02d:%02d", hourOfDay, minutes) + amPm);
+                    }
+                }, currentHour, currentMinute, false);
+
+                timePickerDialog.show();
+            }
+        });
+
+        cancelbtn = findViewById(R.id.cancel_btn);
+        cancelbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Feeder_Configuration_Individual_Diet.this, Feeder_Configuration_General_Geofence.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
